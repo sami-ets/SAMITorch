8b3ad716a2340b91ea0dafd9438d0190554e0931
Pierre-Luc Delisle <github@pierre-luc-delisle.com>
2019-06-01 00:39:47 -0400

    + Added some transformers and tests.

--- samitorch/inputs/transformers.py
+++ samitorch/inputs/transformers.py
@@ -20,6 +20,9 @@ import nrrd
 import math
 import torch
 import os
+import cv2
+
+from typing import Optional, Tuple, Union
 
 from nilearn.image.resampling import resample_to_img
 
@@ -271,7 +274,14 @@ class RemapClassIDs(object):
     Remap the class IDs of a Numpy ndarray to a given list of IDs.
     """
 
-    def __init__(self, initial_ids: list, final_ids: list):
+    def __init__(self, initial_ids: list, final_ids: list) -> None:
+        """
+        Transformer initializer.
+
+        Args:
+            initial_ids (list): A list of integers of the original label values to remap.
+            final_ids (list): A list of integers of final label values.
+        """
         if not isinstance(initial_ids, list) and isinstance(final_ids, list):
             raise TypeError(
                 "Initial and final IDs must be a list of integers, but got {} and {}".format(type(initial_ids),
@@ -303,14 +313,100 @@ class NiftiToDisk(object):
     Write to disk a Nifti image.
     """
 
-    def __init__(self, file_path: str):
+    def __init__(self, file_path: Union[str, list]) -> None:
+        """
+        Transformer initializer.
+
+        Args:
+            file_path (str): The file path to save the Nifti file.
+        """
         self._file_path = file_path
 
-    def __call__(self, image):
-        if isinstance(image, nib.Nifti1Image) or isinstance(image, nib.Nifti2Image):
-            nib.save(image, self._file_path)
+    def __call__(self, nifti_image: Union[nib.Nifti1Image, nib.Nifti2Image, tuple]) -> None:
+        if isinstance(nifti_image, tuple) and isinstance(self._file_path, list):
+            for element, file_name in zip(nifti_image, self._file_path):
+                nib.save(element, file_name)
+        elif isinstance(nifti_image, nib.Nifti1Image) or isinstance(nifti_image, nib.Nifti2Image):
+            nib.save(nifti_image, self._file_path)
         else:
-            raise TypeError("Image type must be Nifti1Image or Nifti2Image, but got {}".format(type(image)))
+            raise TypeError("Image type must be Nifti1Image or Nifti2Image, but got {}".format(type(nifti_image)))
+
+    def __repr__(self):
+        return self.__class__.__name__ + '()'
+
+
+class ToPNGFile(object):
+    """
+       Write to disk a PNG image.
+       """
+
+    def __init__(self, file_path: Union[str, list]) -> None:
+        """
+        Transformer initializer.
+
+        Args:
+            file_path (str): The file path to save the Nifti file.
+        """
+        self._file_path = file_path
+
+    def __call__(self, nd_array: Union[np.ndarray, tuple]) -> None:
+        if isinstance(nd_array, tuple):
+            for element, file_name in zip(nd_array, self._file_path):
+                if element.ndim == 3:
+                    cv2.imwrite(file_name, element[-1, ...])
+                elif element.ndim == 2:
+                    cv2.imwrite(file_name, element)
+                else:
+                    raise TypeError("Only 2D (HxW) or 3D (CxHxW) ndarrays are supported")
+
+        elif isinstance(nd_array, np.ndarray):
+            if not isinstance(nd_array, np.ndarray) and (nd_array.ndim not in [2, 3]):
+                raise TypeError("Only 2D (HxW) or 3D (CxHxW) ndarrays are supported")
+            if nd_array.ndim == 3:
+                cv2.imwrite(self._file_path, nd_array[-1, ...])
+            elif nd_array.ndim == 2:
+                cv2.imwrite(self._file_path, nd_array)
+            else:
+                raise TypeError("Only 2D (HxW) or 3D (CxHxW) ndarrays are supported")
+
+    def __repr__(self):
+        return self.__class__.__name__ + '()'
+
+
+class To2DNifti1Image(object):
+    """
+    Creates a Nifti1Image from a given Numpy ndarray.
+
+    The Numpy arrays are transposed to respect the standard Nifti dimensions (WxHxDxC)
+    """
+
+    def __init__(self, header: Union[nib.Nifti1Header, list] = None) -> None:
+        """
+        Transformer initializer.
+
+        Args:
+            header (:obj:`nibabel.Nifti1Header): The Nifti image header.
+        """
+        if isinstance(header, list):
+            if not len(header) == 2:
+                raise ValueError("List of headers must contain at most 2 elements.")
+
+            self._header = header
+        else:
+            self._header = header
+
+    def __call__(self, nd_array: Union[np.ndarray, tuple]) -> Union[nib.Nifti1Image, tuple]:
+        if isinstance(nd_array, tuple):
+            return (nib.Nifti1Image(nd_array[0].transpose((2, 1, 0)), None,
+                                    self._header[0] if self._header is not None else None),
+                    nib.Nifti1Image(nd_array[1].transpose((2, 1, 0)), None,
+                                    self._header[1] if self._header is not None else None))
+
+        elif isinstance(nd_array, np.ndarray) and isinstance(self._header, nib.Nifti1Header):
+            if not isinstance(nd_array, np.ndarray) and (nd_array.ndim not in [3, 4]):
+                raise TypeError("Only 2D (HxW) or 3D (CxHxW) ndarrays are supported")
+
+            return nib.Nifti1Image(nd_array.transpose((2, 1, 0)), None, self._header)
 
     def __repr__(self):
         return self.__class__.__name__ + '()'
@@ -323,15 +419,32 @@ class ToNifti1Image(object):
     The Numpy arrays are transposed to respect the standard Nifti dimensions (WxHxDxC)
     """
 
-    def __init__(self, file_path: str, header: nib.Nifti1Header):
-        self._file_path = file_path
-        self._header = header
+    def __init__(self, header: Union[nib.Nifti1Header, list, None]) -> None:
+        """
+        Transformer initializer.
 
-    def __call__(self, nd_array):
-        if not isinstance(nd_array, np.ndarray) or (nd_array.ndim not in [3, 4]):
-            raise TypeError("Only 3D (DxHxW) or 4D (CxDxHxW) ndarrays are supported")
+        Args:
+            header (:obj:`nibabel.Nifti1Header): The Nifti image header.
+        """
+        if isinstance(header, list):
+            if not len(header) <= 2:
+                raise ValueError("List of headers must contain at most 2 elements.")
 
-        return nib.Nifti1Image(nd_array.transpose((3, 2, 1, 0)), None, self._header)
+            self._header = header
+        else:
+            self._header = header
+
+    def __call__(self, nd_array: Union[np.ndarray, tuple]) -> Union[nib.Nifti1Image, tuple]:
+
+        if isinstance(nd_array, tuple) and isinstance(self._header, list):
+            return (nib.Nifti1Image(nd_array[0].transpose((3, 2, 1, 0)), None, self._header[0]),
+                    nib.Nifti1Image(nd_array[1].transpose((3, 2, 1, 0)), None, self._header[1]))
+
+        elif isinstance(nd_array, np.ndarray) and isinstance(self._header, nib.Nifti1Header):
+            if not isinstance(nd_array, np.ndarray) or (nd_array.ndim not in [3, 4]):
+                raise TypeError("Only 3D (DxHxW) or 4D (CxDxHxW) ndarrays are supported")
+
+            return nib.Nifti1Image(nd_array.transpose((3, 2, 1, 0)), None, self._header)
 
     def __repr__(self):
         return self.__class__.__name__ + '()'
@@ -344,7 +457,7 @@ class CropToContent(object):
     The content's bounding box is defined by the first non-zero slice in each direction (D, H, W)
     """
 
-    def __call__(self, nd_array: np.ndarray):
+    def __call__(self, nd_array: np.ndarray) -> np.ndarray:
         if not isinstance(nd_array, np.ndarray) or (nd_array.ndim not in [3, 4]):
             raise TypeError("Only 3D (DxHxW) or 4D (CxDxHxW) ndarrays are supported")
 
@@ -357,7 +470,7 @@ class CropToContent(object):
         return self.__class__.__name__ + '()'
 
     @staticmethod
-    def extract_content_bounding_box_from(nd_array: np.ndarray):
+    def extract_content_bounding_box_from(nd_array: np.ndarray) -> Tuple[int, int, int, int, int, int]:
         """
         Computes the D, H, W min and max values defining the content bounding box.
 
@@ -384,11 +497,11 @@ class PadToShape(object):
     Pad an image to a given target shape.
     """
 
-    def __init__(self, target_shape: tuple, padding_value: int = 0):
+    def __init__(self, target_shape: tuple, padding_value: int = 0) -> None:
         self._target_shape = target_shape
         self._padding_value = padding_value
 
-    def __call__(self, nd_array):
+    def __call__(self, nd_array) -> np.ndarray:
         if not isinstance(nd_array, np.ndarray) or (nd_array.ndim not in [3, 4]):
             raise TypeError("Only 3D (DxHxW) or 4D (CxDxHxW) ndarrays are supported")
 
@@ -398,7 +511,7 @@ class PadToShape(object):
         return self.__class__.__name__ + '()'
 
     @staticmethod
-    def apply(nd_array: np.ndarray, target_shape: tuple, padding_value: int):
+    def apply(nd_array: np.ndarray, target_shape: tuple, padding_value: int) -> np.ndarray:
         """
         Apply padding to a Numpy ndarray.
 
@@ -452,3 +565,253 @@ class PadToShape(object):
                        math.floor(deltas[2] / 2):-math.ceil(deltas[2] / 2),
                        math.floor(deltas[3] / 2):-math.ceil(deltas[3] / 2)]
         return nd_array
+
+
+class CropBase(object):
+    """
+    Base class for crop transform.
+    """
+
+    def __init__(self, out_dim: int, output_size: Union[tuple, int, list], threshold: Optional[float] = None) -> None:
+        """
+        Provide the common functionality for RandomCrop2D and RandomCrop3D.
+
+        Args:
+            out_dim (int): The number of dimensions the output must have.
+            output_size (tuple_or_int_or_list): The output size of the cropped imge.
+            threshold (float): Optional threshold on pixel intensities where to crop the image.
+
+        """
+        assert isinstance(output_size, (int, tuple, list))
+        if isinstance(output_size, int):
+            self._output_size = (output_size,)
+            for _ in range(out_dim - 1):
+                self._output_size += (output_size,)
+        else:
+            assert len(output_size) == out_dim
+            self._output_size = output_size
+        self._out_dim = out_dim
+        self._threshold = threshold
+
+    def _get_sample_idxs(self, nd_array: np.ndarray) -> Tuple[int, int, int]:
+        """ get the set of indices from which to sample (foreground) """
+        mask = np.where(
+            nd_array >= (
+                nd_array.mean() if self._threshold is None else self._threshold))  # returns a tuple of length 3
+        c = np.random.randint(0, len(mask[0]))  # choose the set of idxs to use
+        h, w, d = [m[c] for m in mask]  # pull out the chosen idxs
+        return h, w, d
+
+    def __repr__(self):
+        return self.__class__.__name__ + '()'
+
+
+class RandomCrop(CropBase):
+    """
+    Randomly crop a 2-D slice/patch from a 3-D image
+
+    Original source: https://github.com/jcreinhold/niftidataset/blob/master/niftidataset/transforms.py
+    """
+
+    def __init__(self, output_size: Union[int, tuple, list], axis: Optional[int] = 0,
+                 include_neighbors: bool = False, threshold: Optional[float] = None) -> None:
+        """
+        RandomCrop initializer.
+
+        Args:
+            output_size (tuple or int): Desired output size. If int, cube crop is made.
+            axis (int or None): Axis on which should the patch/slice be extracted. Provide None for random axis
+            include_neighbors (bool): Extract 3 neighboring slices instead of just 1
+            threshold (float): Optional threshold on pixel intensities where to crop the image.
+        """
+        if axis is not None:
+            if not 0 <= axis <= 2:
+                raise ValueError("Axis must be in range [0, 2], but got {}".format(axis))
+
+        super().__init__(2, output_size, threshold)
+        self.axis = axis
+        self.include_neighbors = include_neighbors
+
+    def __call__(self, sample: Tuple[np.ndarray, np.ndarray]) -> Tuple[np.ndarray, np.ndarray]:
+        axis = self.axis if self.axis is not None else np.random.randint(0, 3)
+        x, y = sample
+        *cs, h, w, d = x.shape
+        *ct, _, _, _ = x.shape
+        new_h, new_w = self._output_size
+        max_idxs = (np.inf, w - new_h // 2, d - new_w // 2) if axis == 0 else \
+            (h - new_h // 2, np.inf, d - new_w // 2) if axis == 1 else \
+                (h - new_h // 2, w - new_w // 2, np.inf)
+        min_idxs = (-np.inf, new_h // 2, new_w // 2) if axis == 0 else \
+            (new_h // 2, -np.inf, new_w // 2) if axis == 1 else \
+                (new_h // 2, new_w // 2, -np.inf)
+        s = x[0] if len(cs) > 0 else x  # use the first image to determine sampling if multimodal
+        s_idxs = super()._get_sample_idxs(s)
+        idxs = [i if min_i <= i <= max_i else max_i if i > max_i else min_i
+                for max_i, min_i, i in zip(max_idxs, min_idxs, s_idxs)]
+        s = self._get_slice(x, idxs, axis).squeeze()
+        t = self._get_slice(y, idxs, axis).squeeze()
+        if len(cs) == 0 or s.ndim == 2: s = s[np.newaxis, ...]  # add channel axis if empty
+        if len(ct) == 0 or t.ndim == 2: t = t[np.newaxis, ...]
+        return s, t
+
+    def _get_slice(self, nd_array: np.ndarray, idxs: Tuple[int, int, int], axis: int) -> np.ndarray:
+        h, w = self._output_size
+        n = 1 if self.include_neighbors else 0
+        oh = 0 if h % 2 == 0 else 1
+        ow = 0 if w % 2 == 0 else 1
+        i, j, k = idxs
+        s = nd_array[..., i - n:i + 1 + n, j - h // 2:j + h // 2 + oh, k - w // 2:k + w // 2 + ow] if axis == 0 else \
+            nd_array[..., i - h // 2:i + h // 2 + oh, j - n:j + 1 + n, k - w // 2:k + w // 2 + ow] if axis == 1 else \
+                nd_array[..., i - h // 2:i + h // 2 + oh, j - w // 2:j + w // 2 + ow, k - n:k + 1 + n]
+        if self.include_neighbors:
+            s = np.transpose(s, (0, 1, 2)) if axis == 0 else \
+                np.transpose(s, (1, 0, 2)) if axis == 1 else \
+                    np.transpose(s, (2, 0, 1))
+        return s
+
+
+class RandomCrop3D(CropBase):
+    """
+    Randomly crop a 3-D patch from a pair of 3-D images.
+
+    Original source: https://github.com/jcreinhold/niftidataset/blob/master/niftidataset/transforms.py
+    """
+
+    def __init__(self, output_size: Union[tuple, int, list], threshold: Optional[float] = None) -> None:
+        """
+        Random Crop 3D Initializer.
+
+        Args:
+            output_size (tuple or int): Desired output size. If int, cube crop is made.
+            threshold (float): Optional threshold on pixel intensities where to crop the image.
+        """
+        super().__init__(3, output_size, threshold)
+
+    def __call__(self, sample: Tuple[np.ndarray, np.ndarray]) -> Tuple[np.ndarray, np.ndarray]:
+        x, y = sample
+        *cs, h, w, d = x.shape
+        *ct, _, _, _ = y.shape
+        hh, ww, dd = self._output_size
+        max_idxs = (h - hh // 2, w - ww // 2, d - dd // 2)
+        min_idxs = (hh // 2, ww // 2, dd // 2)
+        s = x[0] if len(cs) > 0 else x  # Use the first image to determine sampling if multimodal
+        s_idxs = super()._get_sample_idxs(s)
+        i, j, k = [i if min_i <= i <= max_i else max_i if i > max_i else min_i
+                   for max_i, min_i, i in zip(max_idxs, min_idxs, s_idxs)]
+        oh = 0 if hh % 2 == 0 else 1
+        ow = 0 if ww % 2 == 0 else 1
+        od = 0 if dd % 2 == 0 else 1
+        s = x[..., i - hh // 2:i + hh // 2 + oh, j - ww // 2:j + ww // 2 + ow, k - dd // 2:k + dd // 2 + od]
+        t = y[..., i - hh // 2:i + hh // 2 + oh, j - ww // 2:j + ww // 2 + ow, k - dd // 2:k + dd // 2 + od]
+        if len(cs) == 0: s = s[np.newaxis, ...]  # Add channel axis if empty
+        if len(ct) == 0: t = t[np.newaxis, ...]
+        return s, t
+
+
+class RandomSlice(object):
+    """
+    Take a random 2-D slice from an image given a sample axis.
+
+    Original source: https://github.com/jcreinhold/niftidataset/blob/master/niftidataset/transforms.py
+    """
+
+    def __init__(self, axis: int = 0, div: float = 2) -> None:
+        """
+        Random slice initializer.
+
+        Args:
+            axis (int): Axis on which to take a slice
+            div (float): Divide the mean by this value in the calculation of mask.
+                The higher this value, the more background will be "valid".
+        """
+        if not 0 <= axis <= 2:
+            raise ValueError("Axis must be in range of [0, 2], but got {}".format(axis))
+
+        self._axis = axis
+        self._div = div
+
+    def __call__(self, sample: Tuple[np.ndarray, np.ndarray]) -> Tuple[np.ndarray, np.ndarray]:
+        x, y = sample
+        *cs, _, _, _ = x.shape
+        *ct, _, _, _ = y.shape
+        s = x[0] if len(cs) > 0 else x  # Use the first image to determine sampling if multimodal
+        idx = np.random.choice(self._valid_idxs(s)[self._axis])
+        s = self._get_slice(x, idx)
+        t = self._get_slice(y, idx)
+        if len(cs) == 0: s = s[np.newaxis, ...]  # Add channel axis if empty
+        if len(ct) == 0: t = t[np.newaxis, ...]
+        return s, t
+
+    def __repr__(self):
+        return self.__class__.__name__ + '()'
+
+    def _get_slice(self, nd_array: np.ndarray, idx: int):
+        """
+        Get slices of an nd_array.
+
+        Args:
+            nd_array (:obj:`Numpy.ndarray`): The ndarray on which to extract slices.
+            idx (int): The slice idx.
+
+        Returns:
+            slice: The extracted slice.
+        """
+        s = nd_array[..., idx, :, :] if self._axis == 0 else \
+            nd_array[..., :, idx, :] if self._axis == 1 else \
+                nd_array[..., :, :, idx]
+        return s
+
+    def _valid_idxs(self, nd_array: np.ndarray) -> Tuple[np.ndarray, np.ndarray, np.ndarray]:
+        """
+        Get the set of indices from which to sample (foreground).
+
+        Args:
+             nd_array (:obj:`Numpy.ndarray`): The ndarray on which to extract idxs.
+        """
+        mask = np.where(nd_array > nd_array.mean() / self._div)  # returns a tuple of length 3
+        h, w, d = [np.arange(np.min(m), np.max(m) + 1) for m in mask]  # pull out the valid idx ranges
+        return h, w, d
+
+
+class Normalize(object):
+    """
+    Normalize a Numpy ndarray input.
+    """
+
+    def __init__(self, mean: float, std: float) -> None:
+        """
+        Normalization initializer.
+
+        Args:
+            mean (float): A global mean.
+            std (float): A global standard deviation.
+        """
+        if std <= 0:
+            raise ValueError("Invalid standard deviation, must be greater than 0, but got {}".format(std))
+
+        self._mean = mean
+        self._std = std
+
+    def __call__(self, nd_array: np.ndarray) -> np.ndarray:
+        return (nd_array - self._mean) / self._std
+
+    def __repr__(self):
+        return self.__class__.__name__ + '()'
+
+
+class IntensityScaler(object):
+    """
+    Scale a Numpy ndarray in a default range of 0 to 1.
+    """
+
+    def __init__(self, scale: float = 1) -> None:
+        """
+        Scaler initializer.
+
+        Args:
+            scale (float): The scale of the range.
+        """
+        self._scale = scale
+
+    def __call__(self, nd_array: np.ndarray) -> np.ndarray:
+        return self._scale * ((nd_array - nd_array.min()) / (nd_array.max() - nd_array.min()))
--- tests/inputs/test_transformers.py
+++ tests/inputs/test_transformers.py
@@ -18,13 +18,14 @@ import unittest
 import os
 import nibabel as nib
 import numpy as np
+import collections
+
 from torchvision.transforms import transforms
 
 from hamcrest import *
 
 from samitorch.inputs.transformers import LoadNifti, ToNifti1Image, RemapClassIDs, ApplyMaskToNiftiImage, \
-    ApplyMaskToTensor, \
-    ToNumpyArray
+    ApplyMaskToTensor, ToNumpyArray, RandomCrop, NiftiToDisk, To2DNifti1Image, ToPNGFile
 
 
 class ToNiftiImageTest(unittest.TestCase):
@@ -132,3 +133,152 @@ class RemapClassIDsTest(unittest.TestCase):
 
     def test_should_fail_with_non_integer_class_ids(self):
         assert_that(calling(RemapClassIDs).with_args([1, 2, 3], [1.5, 4, 8]), raises(ValueError))
+
+
+class RandomCropTest(unittest.TestCase):
+    TEST_DATA_FOLDER_PATH = os.path.join(os.path.dirname(__file__), "../data")
+    OUTPUT_DATA_FOLDER_PATH = os.path.join(os.path.dirname(__file__), "../data/generated/randomcrop")
+    VALID_3D_NIFTI_FILE = os.path.join(TEST_DATA_FOLDER_PATH, "T1.nii")
+    VALID_MASK_FILE = os.path.join(TEST_DATA_FOLDER_PATH, "Mask.nii")
+
+    def setUp(self):
+        pass
+
+    def test_transformer_should_return_ndarray(self):
+        nd_array = ToNumpyArray().__call__(self.VALID_3D_NIFTI_FILE)
+        labels = ToNumpyArray().__call__(self.VALID_MASK_FILE)
+
+        transform_ = transforms.Compose([RandomCrop(output_size=32)])
+
+        cropped_nd_array, cropped_label = transform_((nd_array, labels))
+
+        assert_that(cropped_nd_array, instance_of(np.ndarray))
+        assert_that(cropped_label, instance_of(np.ndarray))
+        assert_that(cropped_nd_array.ndim, equal_to(3))
+        assert_that(cropped_label.ndim, equal_to(3))
+        assert_that(cropped_nd_array.shape[0], equal_to(1))
+        assert_that(cropped_label.shape[0], equal_to(1))
+
+    def test_transformer_should_save_files_as_nifti(self):
+        nd_array = ToNumpyArray().__call__(self.VALID_3D_NIFTI_FILE)
+        labels = ToNumpyArray().__call__(self.VALID_MASK_FILE)
+
+        file_names = ["cropped_nd_array.nii", "labels_cropped.nii"]
+
+        transform_ = transforms.Compose([RandomCrop(output_size=32),
+                                         To2DNifti1Image(),
+                                         NiftiToDisk(
+                                             [os.path.join(self.OUTPUT_DATA_FOLDER_PATH, file_path) for file_path in
+                                              file_names])])
+        transform_((nd_array, labels))
+
+        for file_name in file_names:
+            assert_that(os.path.exists(os.path.join(self.OUTPUT_DATA_FOLDER_PATH, file_name)))
+
+    def test_transformer_should_save_files_as_png(self):
+        nd_array = ToNumpyArray().__call__(self.VALID_3D_NIFTI_FILE)
+        labels = ToNumpyArray().__call__(self.VALID_MASK_FILE)
+
+        file_names = ["cropped_nd_array.png", "labels_cropped.png"]
+
+        transform_ = transforms.Compose([RandomCrop(output_size=32),
+
+                                         ToPNGFile(
+                                             [os.path.join(self.OUTPUT_DATA_FOLDER_PATH, file_path) for file_path in
+                                              file_names])])
+        transform_((nd_array.astype(np.float32), 75 * labels.astype(np.uint8)))
+
+        for file_name in file_names:
+            assert_that(os.path.exists(os.path.join(self.OUTPUT_DATA_FOLDER_PATH, file_name)))
+
+    def test_transformer_should_save_with_2D_ndarray(self):
+        nd_array = ToNumpyArray().__call__(self.VALID_3D_NIFTI_FILE)
+        labels = ToNumpyArray().__call__(self.VALID_MASK_FILE)
+
+        file_names = ["cropped_nd_array.png", "labels_cropped.png"]
+
+        transform_ = transforms.Compose([RandomCrop(output_size=32)])
+
+        cropped_nd_array, cropped_label = transform_((nd_array, labels))
+
+        ToPNGFile(file_names[0]).__call__(cropped_nd_array[0, ...])
+
+
+class ToPNGFileTest(unittest.TestCase):
+    TEST_DATA_FOLDER_PATH = os.path.join(os.path.dirname(__file__), "../data")
+    OUTPUT_DATA_FOLDER_PATH = os.path.join(os.path.dirname(__file__), "../data/generated/ToPNGFile")
+    PNG_FILE_2D = os.path.join(OUTPUT_DATA_FOLDER_PATH, "ToPNGFile2D.png")
+    PNG_FILE_3D = os.path.join(OUTPUT_DATA_FOLDER_PATH, "ToPNGFile3D.png")
+    PNG_FILE_4D = os.path.join(OUTPUT_DATA_FOLDER_PATH, "ToPNGFile4D.png")
+    PNG_TUPLE_2D = [os.path.join(OUTPUT_DATA_FOLDER_PATH, "ToPNGFileTupleHxW_1.png"),
+                    os.path.join(OUTPUT_DATA_FOLDER_PATH, "ToPNGFileTupleHxW_2.png")]
+    PNG_TUPLE_3D = [os.path.join(OUTPUT_DATA_FOLDER_PATH, "ToPNGFileTupleCxHxW_1.png"),
+                    os.path.join(OUTPUT_DATA_FOLDER_PATH, "ToPNGFileTupleCxHxW_2.png")]
+    PNG_TUPLE_4D = [os.path.join(OUTPUT_DATA_FOLDER_PATH, "ToPNGFileTupleCxDxHxW_1.png"),
+                    os.path.join(OUTPUT_DATA_FOLDER_PATH, "ToPNGFileTupleCxDxHxW_2.png")]
+    ALL = [PNG_FILE_2D, PNG_FILE_3D, PNG_FILE_4D, PNG_TUPLE_2D, PNG_TUPLE_3D, PNG_TUPLE_4D]
+
+    def _flatten(self, l):
+        for el in l:
+            if isinstance(el, collections.Iterable) and not isinstance(el, (str, bytes)):
+                yield from self._flatten(el)
+            else:
+                yield el
+
+    def setUp(self):
+        all_files = self._flatten(self.ALL)
+        for element in all_files:
+            if os.path.exists(element):
+                os.remove(element)
+            else:
+                print('File does not exists')
+
+        assert_that(len(os.listdir(self.OUTPUT_DATA_FOLDER_PATH)), is_(0))
+
+        print("Files deleted.")
+
+    def test_should_raise_exception_when_passing_4D_ndarray(self):
+        nd_array = np.random.randint(0, 1000, size=(1, 32, 32, 32))
+
+        transform_ = ToPNGFile(self.PNG_FILE_4D)
+
+        assert_that(calling(transform_.__call__).with_args(nd_array), raises(TypeError))
+
+    def test_should_raise_exception_when_passing_tuple_of_4D_ndarray(self):
+        nd_array = np.random.randint(0, 1000, size=(1, 32, 32, 32))
+
+        transform_ = ToPNGFile(self.PNG_TUPLE_4D)
+
+        assert_that(calling(transform_.__call__).with_args((nd_array, nd_array)), raises(TypeError))
+
+    def test_should_pass_when_passing_3D_ndarray(self):
+        nd_array = np.random.randint(0, 1000, size=(1, 32, 32))
+
+        ToPNGFile(self.PNG_FILE_3D).__call__(nd_array)
+
+        assert_that(os.path.exists(os.path.join(self.OUTPUT_DATA_FOLDER_PATH, self.PNG_FILE_3D)))
+
+    def test_should_pass_when_passing_2D_ndarray(self):
+        nd_array = np.random.randint(0, 1000, size=(32, 32))
+
+        ToPNGFile(self.PNG_FILE_2D).__call__(nd_array)
+
+        assert_that(os.path.exists(os.path.join(self.OUTPUT_DATA_FOLDER_PATH, self.PNG_FILE_2D)))
+
+    def test_should_pass_when_passing_tuple_2D_nd_array(self):
+
+        nd_array = np.random.randint(0, 1000, size=(32, 32))
+
+        ToPNGFile(self.PNG_TUPLE_2D).__call__((nd_array, nd_array))
+
+        for file_name in self.PNG_TUPLE_2D:
+            assert_that(os.path.exists(os.path.join(self.OUTPUT_DATA_FOLDER_PATH, file_name)))
+
+    def test_should_pass_when_passing_tuple_3D_nd_array(self):
+
+        nd_array = np.random.randint(0, 1000, size=(1, 32, 32))
+
+        ToPNGFile(self.PNG_TUPLE_3D).__call__((nd_array, nd_array))
+
+        for file_name in self.PNG_TUPLE_3D:
+            assert_that(os.path.exists(os.path.join(self.OUTPUT_DATA_FOLDER_PATH, file_name)))
