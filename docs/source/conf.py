816fb4d80593290bc0ec371b66bcd4eeb322c737
Pierre-Luc Delisle <github@pierre-luc-delisle.com>
2019-07-02 15:25:08 -0400

    * Version up

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ copyright = '2019, Pierre-Luc Delisle, Benoit Anctil-Robitaille'
 author = 'Pierre-Luc Delisle, Benoit Anctil-Robitaille'
 
 # The full version, including alpha/beta/rc tags
-release = '0.1.25'
+release = '0.1.26'
 
 # -- General configuration ---------------------------------------------------
 
