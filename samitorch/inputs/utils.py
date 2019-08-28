ac7baa04c986fa5a0b8ac25eedda4a41f9ff0ef0
Pierre-Luc Delisle <github@pierre-luc-delisle.com>
2019-08-28 16:38:41 -0400

    * Fixed collate function to match Kerosene.

--- samitorch/inputs/utils.py
+++ samitorch/inputs/utils.py
@@ -13,15 +13,16 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 #  ==============================================================================
+import torch
 
 from samitorch.inputs.batch import ImageBatch, PatchBatch
 
 
 def sample_collate(batch: list):
     batch = ImageBatch(samples=batch)
-    return batch.x, (batch.y, batch.dataset_id)
+    return batch.x, torch.Tensor().new_tensor([batch.y, batch.dataset_id])
 
 
 def patch_collate(batch: list):
     batch = PatchBatch(samples=batch)
-    return batch.x, (batch.y, batch.dataset_id)
+    return batch.x, torch.Tensor().new_tensor([batch.y, batch.dataset_id])
