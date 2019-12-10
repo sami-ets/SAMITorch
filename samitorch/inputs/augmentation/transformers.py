c8982251ac8c23992969b8ba84b73fef335648f7
Pierre-Luc Delisle <github@pierre-luc-delisle.com>
2019-12-10 15:40:29 -0500

    * Fixed docstring

--- samitorch/inputs/augmentation/transformers.py
+++ samitorch/inputs/augmentation/transformers.py
@@ -36,19 +36,19 @@ class AddNoise(object):
                 distributed noise, 'rician' for Rice-distributed noise (default) or 'rayleigh' for a Rayleigh
                  distribution.
 
-            Notes:
-                SNR is defined here, following [1]_, as ``S0 / sigma``, where ``sigma`` is
-                the standard deviation of the two Gaussian distributions forming the real
-                and imaginary components of the Rician noise distribution (see [2]_).
+        Notes:
+            SNR is defined here, following [1]_, as ``S0 / sigma``, where ``sigma`` is
+            the standard deviation of the two Gaussian distributions forming the real
+            and imaginary components of the Rician noise distribution (see [2]_).
 
-            References:
-                .. [1] Descoteaux, Angelino, Fitzgibbons and Deriche (2007) Regularized,
-                       fast and robust q-ball imaging. MRM, 58: 497-510
-                .. [2] Gudbjartson and Patz (2008). The Rician distribution of noisy MRI
-                       data. MRM 34: 910-914.
+        References:
+            .. [1] Descoteaux, Angelino, Fitzgibbons and Deriche (2007) Regularized,
+                   fast and robust q-ball imaging. MRM, 58: 497-510
+            .. [2] Gudbjartson and Patz (2008). The Rician distribution of noisy MRI
+                   data. MRM 34: 910-914.
 
-            Source:
-                https://github.com/nipy/dipy/blob/108cd1137386462cda08438cddee15285131af08/dipy/sims/voxel.py#L82
+        Source:
+            https://github.com/nipy/dipy/blob/108cd1137386462cda08438cddee15285131af08/dipy/sims/voxel.py#L82
         """
         if isinstance(input, np.ndarray):
 
